85797a293de8a24c3b52701f4660d699ec27989e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-12 12:45:40 +0100

    Create popup.py

---
+++ functions/popup.py
@@ -0,0 +1,73 @@
+import streamlit as st
+
+import pandas as pd
+import datetime
+from datetime import datetime, timedelta, date
+
+from constants import *
+
+def popup_polygons(row,df_point):
+    
+    i = row
+
+    datum=df_point['datum'].iloc[i] 
+    specie = df_point['species'].iloc[i] 
+    opmerking=df_point['opmerking'].iloc[i]       
+
+    left_col_color = "#19a7bd"
+    right_col_color = "#f2f0d3"
+    
+    html = """<!DOCTYPE html>
+    <html>
+    <table style="height: 126px; width: 300;">
+    <tbody>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Soort</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(specie) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Opmerking</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(opmerking) + """
+    </tr>
+    </tbody>
+    </table>
+    </html>
+    """
+    return html
+
+def popup_points(row,df_point):
+    
+    i = row
+
+    datum=df_point['datum'].iloc[i] 
+    specie = df_point['species'].iloc[i] 
+    opmerking=df_point['opmerking'].iloc[i]       
+
+    left_col_color = "#19a7bd"
+    right_col_color = "#f2f0d3"
+    
+    html = """<!DOCTYPE html>
+    <html>
+    <table style="height: 126px; width: 300;">
+    <tbody>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Soort</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(specie) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Opmerking</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(opmerking) + """
+    </tr>
+    </tbody>
+    </table>
+    </html>
+    """
+    return html
